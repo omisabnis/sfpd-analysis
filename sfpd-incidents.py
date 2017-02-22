6c856e66e927b8696ca70bec39fcf7ef2c65aad4
Omkar Sabnis <omisabnis@gmail.com>
2017-02-22 09:12:35 -0500

    Update sfpd-incidents.py

--- sfpd-incidents.py
+++ sfpd-incidents.py
@@ -15,33 +15,34 @@ csvSchema = StructType([StructField('IncidentNum',LongType(),True),
                        StructField('Location',StringType(),True),
                        StructField('PdId',LongType(),True)])
                    
-df = spark.read.csv('SFPD_Incidents_-_from_1_January_2003.csv', header=True, schema=csvSchema).cache() 
+df = spark.read.csv('SFPD_Incidents_-_from_1_January_2003.csv', header=True, schema=csvSchema) 
 
 #Concatenating Date and Time column
-cdf = df.select('IncidentNum','Category','Description','DayOfWeek',concat(col('Date'), lit(' '), col('Time')).alias('DateTime'),'PdDistrict','Resolution','Address','Latitude','Longitude','Location','PdId')
-cdf.printSchema()
+concatdf = df.select('IncidentNum','Category','Description','DayOfWeek',concat(col('Date'), lit(' '), col('Time')).alias('DateTime'),'PdDistrict','Resolution','Address','Latitude','Longitude','Location','PdId')
+concatdf.printSchema()
 from_pattern1 = 'MM/dd/yyyy hh:mm'
 
 #converting the datatype of the new column to timestamp
-nff = cdf.withColumn('DateTS', unix_timestamp(cdf['DateTime'], from_pattern1).cast('timestamp')).drop('DateTime')
-nff.columns
-nff.createOrReplaceTempView('sfpd')
+dfTS = concatdf.withColumn('DateTS', unix_timestamp(concatdf['DateTime'], from_pattern1).cast('timestamp')).drop('DateTime')
+dfTS.cache()
+dfTS.columns
+dfTs.createOrReplaceTempView('sfpd')
 spark.catalog.cacheTable('sfpd')
 spark.table('sfpd').count()
-display(nff.select('Category').distinct())
+display(dfTS.select('Category').distinct())
 
 #displays the total incidents recorded for each category
-display(nff.select('Category').groupBy('Category').count().orderBy('count', ascending=False))
+display(dfTS.select('Category').groupBy('Category').count().orderBy('count', ascending=False))
 
 #displays the total incidents recorded per year
-display(nff.groupBy(year('DateTS')).count().orderBy('year(DateTS)'))
+display(dfTS.groupBy(year('DateTS')).count().orderBy('year(DateTS)'))
 
 #incidents recorded in the month of January 2017
-display(nff.filter(year('DateTS') == '2017').filter(dayofyear('DateTS') <= 31).groupBy(dayofyear('DateTS')).count().orderBy('dayofyear(DateTS)'))
+display(dfTS.filter(year('DateTS') == '2017').filter(dayofyear('DateTS') <= 31).groupBy(dayofyear('DateTS')).count().orderBy('dayofyear(DateTS)'))
 
 #day of week that records maximum incidents
-display(nff.select('DayOfWeek').groupBy('DayOfWeek').count().orderBy('count',ascending=False))
+display(dfTS.select('DayOfWeek').groupBy('DayOfWeek').count().orderBy('count',ascending=False))
 
 #incidents recorded from noon to midnight
-display(nff.filter(hour('DateTS') >= 12).groupBy(hour('DateTS')).count().orderBy('hour(DateTS)',ascending=True))
+display(dfTS.filter(hour('DateTS') >= 12).groupBy(hour('DateTS')).count().orderBy('hour(DateTS)',ascending=True))
 
